7e33938e7802c7e32a9f14a1633bef5171c30c00
BIGWOJ <wojtek.latos@interia.pl>
2024-10-30 09:48:29 +0100

    .

--- Map puzzle/puzzle_js.js
+++ Map puzzle/puzzle_js.js
@@ -1,11 +1,16 @@
-const map = L.map('map_box').setView([53.432826892328116, 14.548091452802913], 15);
-const map_ss = document.getElementById('download_icon');
-
+let map = L.map('map_box').setView([53.432826892328116, 14.548091452802913], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-function take_screenshot() {
+document.getElementById("download_icon").addEventListener("click", function() {
+    leafletImage(map, function (err, canvas) {
+        let rasterMap = document.getElementById("raster_map");
+        let rasterContext = rasterMap.getContext("2d");
 
-}
+        rasterContext.drawImage(canvas, 0, 0, rasterMap.width, rasterMap.height);
+    });
+});
+
+
