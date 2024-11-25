0a85596b467349d48550d6027e0cea655b47d819
BIGWOJ <wojtek.latos@interia.pl>
2024-11-25 12:44:37 +0100

    Start of the typescript project

---
+++ Type script/dist/script.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!*******************!*\
+  !*** ./script.ts ***!
+  \*******************/
+
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  var theme_button = document.querySelector('.theme_button');
+  var body = document.querySelector('body');
+  if (theme_button && body) {
+    console.log("dziala");
+    //     const msg: string = "aaaaaaaaaa";
+    //     alert(msg);
+    //     theme_button.addEventListener('click', (event) => {
+    //     body.style.backgroundColor = 'red';
+    //     })
+  }
+});
+/******/ })()
+;
---
+++ Type script/script.ts
@@ -0,0 +1,14 @@
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    const theme_button = document.querySelector('.theme_button');
+    const body = document.querySelector('body');
+    if (theme_button && body) {
+        console.log("dziala");
+        //     const msg: string = "aaaaaaaaaa";
+    //     alert(msg);
+    //     theme_button.addEventListener('click', (event) => {
+    //     body.style.backgroundColor = 'red';
+    //     })
+    }
+})
