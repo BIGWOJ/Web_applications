5b4c4f58bb08be0a156b7461026ec1857cc2cc66
BIGWOJ <wojtek.latos@interia.pl>
2024-11-29 23:55:21 +0100

    ''

--- Type script/dist/script.js
+++ Type script/dist/script.js
@@ -5,17 +5,26 @@
   \*******************/
 
 
+var default_theme = 'page1.css';
+var second_theme = 'page2.css';
+var current_theme = 'default';
 window.addEventListener('DOMContentLoaded', function (event) {
   var theme_button = document.querySelector('.theme_button');
   var body = document.querySelector('body');
-  if (theme_button && body) {
-    console.log("dziala");
-    //     const msg: string = "aaaaaaaaaa";
-    //     alert(msg);
-    //     theme_button.addEventListener('click', (event) => {
-    //     body.style.backgroundColor = 'red';
-    //     })
-  }
+  var second_theme_link = document.createElement('link');
+  // if (theme_button && body) {
+  //     theme_button.addEventListener('click', (event) => {
+  //         if (current_theme === 'default') {
+  //             body.style.backgroundImage = 'url("sauna_background.jpg")';
+  //             current_theme = 'gray_background';
+  //         }
+  //         else {
+  //             body.style.backgroundImage = 'none';
+  //             body.style.backgroundColor = '#372f37';
+  //             current_theme = 'default';
+  //         }
+  //     });
+  // }
 });
 /******/ })()
 ;
--- Type script/script.ts
+++ Type script/script.ts
@@ -1,14 +1,30 @@
-
+let default_theme = 'page1.css';
+let second_theme = 'page2.css';
+let current_theme = 'default';
 
 window.addEventListener('DOMContentLoaded', (event) => {
     const theme_button = document.querySelector('.theme_button');
     const body = document.querySelector('body');
-    if (theme_button && body) {
-        console.log("dziala");
-        //     const msg: string = "aaaaaaaaaa";
-    //     alert(msg);
+
+    const second_theme_link = document.createElement('link');
+
+
+
+
+
+
+
+    // if (theme_button && body) {
     //     theme_button.addEventListener('click', (event) => {
-    //     body.style.backgroundColor = 'red';
-    //     })
-    }
+    //         if (current_theme === 'default') {
+    //             body.style.backgroundImage = 'url("sauna_background.jpg")';
+    //             current_theme = 'gray_background';
+    //         }
+    //         else {
+    //             body.style.backgroundImage = 'none';
+    //             body.style.backgroundColor = '#372f37';
+    //             current_theme = 'default';
+    //         }
+    //     });
+    // }
 })
